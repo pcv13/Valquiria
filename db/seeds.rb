aebbd09f5ace311b8363df59b635ddc7368efb50
Pablo <Pcampos321hhotmail.com>
2016-09-21 17:36:00 +0200

    seed actualizadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-games = Game.create([{name: "LLamada de cthulhu",image:"#"},{name: "Dragones y Mazmorras",image:"#"}])
+games = Game.create([
+    {name: "LLamada de cthulhu",image:"http://www.elroldesiempreonline.com/imagenes/juegos/galerias/la-llamada-de-cthulhu3.jpg"},
+    {name: "Savage Worlds",image:"http://www.elroldesiempreonline.com/imagenes/juegos/savage-worlds.jpg"},
+    {name: "GURPS",image:"http://www.sjgames.com/wallpaper/img/GURPS4E800.jpg.gif"},
+    {name: "The World of Darkness (Vampire)",image:"http://www.juegosderoles.com/wp-content/uploads/2013/09/vampiro2.jpg"},
+    {name: "The World of Darkness (Mage)",image:"http://www.juegosderoles.com/wp-content/uploads/2013/09/vampiro2.jpg"},
+    {name: "The World of Darkness (Warewolf)",image:"http://www.juegosderoles.com/wp-content/uploads/2013/09/vampiro2.jpg"},
+    {name: "Traveller",image:"http://2.bp.blogspot.com/-r_E-KPIOJJI/VAjiNc1rcVI/AAAAAAAAAJ8/za_ZygNatjU/s1600/Marca%2BEspiral.PNG"},
+   {name: "Fiasco",image:"https://rolentuidioma.files.wordpress.com/2014/11/fiasco-back-cover2.png"}])
 
 player = Player.create([{name: "Rocio", email: "Rocio@mail.com",password: "12345678" },
     {name: "Pablo", email: "Pablo@mail.com",password: "12345678" },
