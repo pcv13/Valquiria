3e64dd922cd0f953bd13ab9e7513a9c601410b4f
Pablo <Pcampos321hhotmail.com>
2016-09-29 10:04:48 +0200

    MVP funcionando 00

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,4 +4,9 @@ class Match < ApplicationRecord
   has_many :players,through: :match_players
   self.per_page = 10
 
+
+
+
+ 	  validates :name,  presence: true
+   	validates :star_time,  presence: true
 end
